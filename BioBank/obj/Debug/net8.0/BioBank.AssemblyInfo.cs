e519fc395f58801ce4276c9c7a252bd619ce2499
apick98 <32792153+apick98@users.noreply.github.com>
2024-01-06 14:22:06 +0000

    Made sure the headers for Disease Term display correctly

--- BioBank/obj/Debug/net8.0/BioBank.AssemblyInfo.cs
+++ BioBank/obj/Debug/net8.0/BioBank.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BioBank")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39006be7bf37e58cc81c30e221e95b210ee2b3bd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+954e38ffda395d2221f24a4041bdccda380f10cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("BioBank")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BioBank")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
