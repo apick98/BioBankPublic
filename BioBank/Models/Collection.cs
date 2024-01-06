483866c59fa9e39c247d49af0c36ab9af05ca4b0
apick98 <32792153+apick98@users.noreply.github.com>
2024-01-06 12:52:20 +0000

    Created Models folder and Collection class file

---
+++ BioBank/Models/Collection.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace BioBank.Models
+{
+    public class Collection
+    {
+        public int ID { get; set; }
+        public required string DiseaseTerm { get; set; }
+        public required string Title { get; set; }
+    }
+}
