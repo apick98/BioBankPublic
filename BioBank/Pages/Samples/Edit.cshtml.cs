93285f0926f024b5bb7e0c49b5f9444750888041
apick98 <32792153+apick98@users.noreply.github.com>
2024-01-07 17:23:08 +0000

    Added edit sample backend functionality and the redirect after a successful edit works

--- BioBank/Pages/Samples/Edit.cshtml.cs
+++ BioBank/Pages/Samples/Edit.cshtml.cs
@@ -1,10 +1,7 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using BioBank.Data;
 using BioBank.Models;
@@ -13,9 +10,9 @@ namespace BioBank.Pages.Samples
 {
     public class EditModel : PageModel
     {
-        private readonly BioBank.Data.BioBankContext _context;
+        private readonly BioBankContext _context;
 
-        public EditModel(BioBank.Data.BioBankContext context)
+        public EditModel(BioBankContext context)
         {
             _context = context;
         }
@@ -33,52 +30,38 @@ namespace BioBank.Pages.Samples
                 return NotFound();
             }
 
-            var sample =  await _context.Samples.FirstOrDefaultAsync(m => m.SampleID == id);
-            if (sample == null)
+            Sample = await _context.Samples.FindAsync(id);
+
+            if (Sample == null)
             {
                 return NotFound();
             }
             else
             {
-                Sample = sample;
                 CollectionID = Sample.CollectionID;
             }
             return Page();
         }
 
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see https://aka.ms/RazorPagesCRUD.
-        public async Task<IActionResult> OnPostAsync()
+        public async Task<IActionResult> OnPostAsync(int id)
         {
-            if (!ModelState.IsValid)
+            var sampleToUpdate = await _context.Samples.FindAsync(id);
+
+            if (sampleToUpdate == null)
             {
-                return Page();
+                return NotFound();
             }
 
-            _context.Attach(Sample).State = EntityState.Modified;
-
-            try
+            if (await TryUpdateModelAsync<Sample>(
+                sampleToUpdate,
+                "sample",
+                s => s.DonorCount, s => s.MaterialType, s => s.LastUpdated))
             {
                 await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!SampleExists(Sample.SampleID))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
+                return RedirectToPage("/Collections/Details", new { id = sampleToUpdate.CollectionID });
             }
 
-            return RedirectToPage("./Index");
-        }
-
-        private bool SampleExists(int id)
-        {
-            return _context.Samples.Any(e => e.SampleID == id);
+            return RedirectToPage("/Collections/Details", new { id = CollectionID });
         }
     }
 }
